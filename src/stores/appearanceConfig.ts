5ddc9d1988533d6a5b6fe4be54fc8b94823fd6e4
kono-dada <121090029@link.cuhk.edu.cn>
2025-07-22 01:54:16 +0800

    增加一些小装饰

--- src/stores/appearanceConfig.ts
+++ src/stores/appearanceConfig.ts
@@ -4,9 +4,11 @@ import { ref } from 'vue'
 export const useAppearanceConfigStore = defineStore('appearanceConfig', () => {
   const petSize = ref(100) // Default size in pixels
   const opacity = ref(1) // Default transparency
+  const showDecorations = ref(true) // Default to showing decorations
 
   return {
     petSize,
     opacity,
+    showDecorations,
   }
 });
