bcef0409f480767f71d3331128e03f8013b00674
kono-dada <121090029@link.cuhk.edu.cn>
2025-08-05 12:28:01 +0800

    emotions换成枚举类

--- src/constants/emotions.ts
+++ src/constants/emotions.ts
@@ -1,10 +1,5 @@
-import type { EmotionName } from '../types/emotion';
+import { EmotionName } from '../types/emotion';
 
-export const EMOTIONS: EmotionName[] = [
-  "正常", "高兴", "伤心", "生气", "害怕", 
-  "惊讶", "厌恶", "羞愤", "微笑", "担心",
-  "调皮", "慌张", "紧张", "认真", "无奈",
-  "心动", "羞耻", "自信", "疑惑", "高潮脸", "欲求不满"
-];
+export const EMOTIONS: EmotionName[] = Object.values(EmotionName);
 
-export const DEFAULT_EMOTION: EmotionName = "正常";
+export const DEFAULT_EMOTION: EmotionName = EmotionName.正常;
