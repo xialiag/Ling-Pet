0220d63051ff2ca313117bdeb98c5ae25e0e3d01
kono-dada <121090029@link.cuhk.edu.cn>
2025-08-15 11:40:37 +0800

    fix: 将vuetify配置移至main.ts.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,20 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from "./plugins/vuetify";
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Material Design Icons 样式
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    // 确保默认图标集设置为 'mdi'
+    defaultSet: 'mdi',
+  },
+})
 
 
 // 创建Vue应用实例，注册路由器并挂载到DOM元素
