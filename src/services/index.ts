2cf0a614724ee113e959e11e0bd7aa481692012d
kono-dada <121090029@link.cuhk.edu.cn>
2025-10-02 19:03:17 +0800

    fix: 删除无用的index.ts

--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-// 服务导出文件
-export { 
-  live2dModelService, 
-  initializeLive2DModels, 
-  validateModel 
-} from './live2d/live2dModelService'
-export { showNotification } from './notificationService'
